cf9cddbf2e9d40b8a55e90d7f3c9bf8819e77fc7
Jihyeon Park <108461149+milhaud1201@users.noreply.github.com>
2022-07-29 13:03:56 +0900

    Update components.py

--- components/components.py
+++ components/components.py
@@ -7,4 +7,6 @@ from PIL import Image
 
 def movie_info_component(movie_info):
   st.subheader(f"{movie_info['title']} ({movie_info['date']})")
-  
+  st.caption(
+    f"부제: {movie_info['subtitle'] if movie_info['subtitle'] else '정보가 없습니다.'}"
+  )
